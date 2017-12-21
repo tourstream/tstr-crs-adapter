309fb5faa304b3b28f2a7ca28944432e468f448e
Steve Reichenbach <steve.reichenbach@tourstream.eu>
2017-12-21 17:01:43 +0100

    BM-214 restructure test file
    BM-214 clean up a little bit

--- tests/manual/assets/helpers.js
+++ tests/manual/assets/helpers.js
@@ -1,172 +1,208 @@
-function removeChildrenFromElement(elementSelector, childSelector) {
-    var element = window.document.getElementById(elementSelector);
-    var children = element.getElementsByClassName(childSelector);
+(function() {
+    "use strict";
 
-    [].forEach.call(children, function (child) {
-        element.removeChild(child);
-    });
-}
+    const crsAdapter = new window.UbpCrsAdapter.default({debug: true});
+    const connectionOptionsForm = document.getElementById('connect-options');
+    const productForm = document.getElementById('product');
+    const crsSelectionButtons = document.getElementById('crs-selection').getElementsByTagName('button');
+    const productSelectionButtons = document.getElementById('product-selection').getElementsByTagName('button');
+    const formFieldTemplate = document.getElementById('form-field-template');
+    const reportBlock = window.document.getElementById('report');
+    const baseProductFields = window.document.getElementById('base-product-form-fields');
 
-function syntaxHighlight(json) {
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    init();
 
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
+    function init() {
+        initSelectionButtons();
+        initFormFields();
+        resetForm(connectionOptionsForm);
+        resetForm(productForm);
+    }
+
+    function initSelectionButtons() {
+        initCrsSelection();
+        initProductSelection();
+    }
+
+    function initCrsSelection() {
+        Array.from(crsSelectionButtons).forEach(function(button) {
+            button.onclick = function(event) {
+                if (button.type === 'submit') {
+                    if (button.name === 'connect') {
+                        connectToCrs();
+                    }
+
+                    return;
+                }
+
+                resetForm(connectionOptionsForm);
+                selectTemplate(connectionOptionsForm, event.target.value);
+                selectTemplate(productForm, 'base-product');
+            };
+        });
+    }
+
+    function connectToCrs() {
+        try {
+            if (!connectionOptionsForm.type) {
+                throw new Error('no CRS selected');
             }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
+
+            let data = {};
+
+            Object.keys(connectionOptionsForm).forEach(function(key) {
+                if (!connectionOptionsForm[key].name || connectionOptionsForm[key].value === '') return;
+
+                setValueToPropertyPath(data, connectionOptionsForm[key].name, connectionOptionsForm[key].value);
+            });
+
+            delete data.type;
+
+            crsAdapter
+                .connect(connectionOptionsForm.type.value, data)
+                .then(log('connected to ' + connectionOptionsForm.type.value), log);
+        } catch (e) {
+            log(e);
         }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
-
-function log(text) {
-    var stringified = JSON.stringify(text, void 0, 4);
-
-    if (stringified === '{}') {
-        stringified = text.toString();
     }
 
-    var highlighted = syntaxHighlight(stringified.replace(/\\n/g, '\n'));
+    function initProductSelection() {
+        Array.from(productSelectionButtons).forEach(function(button) {
+            button.onclick = function(event) {
+                if (button.type === 'submit') {
+                    switch (button.name) {
+                        case 'get': {
+                            getData();
+                            break;
+                        }
+                        case 'send': {
+                            sendData();
+                            break;
+                        }
+                        case 'exit': {
+                            doExit();
+                            break;
+                        }
+                    }
 
-    reportBlock.innerHTML += '<pre>' + highlighted + '</pre>';
-}
+                    return;
+                }
 
-var reportBlock = window.document.getElementById('report');
-var carServiceTemplate = window.document.getElementById('car-service-template');
-var hotelServiceTemplate = window.document.getElementById('hotel-service-template');
-var roundTripServiceTemplate = window.document.getElementById('round-trip-service-template');
-var camperServiceTemplate = window.document.getElementById('camper-service-template');
-var connectOptionsMap = {
-    toma: window.document.getElementById('toma-connect-template'),
-    toma2: window.document.getElementById('toma2-connect-template'),
-    bewotec: window.document.getElementById('bewotec-connect-template'),
-    traffics: window.document.getElementById('traffics-connect-template'),
-};
-var form = document.getElementById('data-form');
-var formConnectOptions = document.getElementById('connect-options');
-var adapter = new window.UbpCrsAdapter.default({debug: true});
-var selectedCrs;
-
-document.getElementById('crs-selector').addEventListener('change', function (event) {
-    if (!event.target.selectedIndex) {
-        return;
+                resetForm(productForm);
+                selectTemplate(productForm, 'base-product');
+                selectTemplate(productForm, event.target.value);
+            };
+        });
     }
 
-    selectedCrs = event.target.value;
-
-    removeChildrenFromElement('connect-options', 'connect-option');
-
-    if (connectOptionsMap[selectedCrs]) {
-        formConnectOptions.appendChild(connectOptionsMap[selectedCrs].cloneNode(true));
-    }
-});
-
-document.getElementById('connect').addEventListener('click', function () {
-    if (!selectedCrs) {
-        return;
+    function resetForm(form) {
+        form.innerHTML = '';
     }
 
-    try {
-        var config = {};
+    function selectTemplate(form, type) {
+        let template = document.getElementById(type + '-form-fields');
 
-        Object.keys(formConnectOptions).forEach(function (key) {
-            config[formConnectOptions[key].name] = formConnectOptions[key].value;
+        if (template) {
+            form.appendChild(template.cloneNode(true));
+        }
+    }
+
+    function getData() {
+        try {
+            crsAdapter.getData().then(log).catch(log);
+        } catch (e) {
+            log(e);
+        }
+    }
+
+    function sendData() {
+        let data = {};
+
+        Object.keys(productForm).forEach(function(key) {
+            if (!productForm[key].name || productForm[key].value === '') return;
+
+            setValueToPropertyPath(data, productForm[key].name, productForm[key].value);
         });
 
-        adapter.connect(selectedCrs, config).catch(log);
-    } catch (e) {
-        log(e);
+        crsAdapter.setData(data).catch(log);
     }
-});
 
-document.getElementById('get-data-btn').addEventListener('click', function () {
-    try {
-        adapter.getData().then(log).catch(log);
-    } catch (e) {
-        log(e);
+    function log(text) {
+        let stringified = JSON.stringify(text, void 0, 4) || '';
+
+        if (stringified === '{}') {
+            stringified = text.toString();
+        }
+
+        let highlighted = syntaxHighlight(stringified.replace(/\\n/g, '\n'));
+
+        reportBlock.innerHTML = '<pre>' + highlighted + '</pre>';
     }
-});
 
-document.getElementById('set-data-btn').addEventListener('click', function () {
-    try {
-        var data = {};
+    function syntaxHighlight(json) {
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
-        Object.keys(form).forEach(function(key) {
-            if (!form[key].name || form[key].value === '') return;
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            let cls = 'number';
 
-            setValueToPropertyPath(data, form[key].name, form[key].value);
-        });
-
-        log('collected data');
-        log(data);
-
-        if (selectedCrs === 'bm') {
-            var decision = window.prompt('Send [1] for a direct checkout else it will be added to the basket');
-
-            if (decision === '1') {
-                adapter.directCheckout(data).catch(log);
-            } else {
-                adapter.addToBasket(data).catch(log);
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
             }
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+    }
+
+    function setValueToPropertyPath(object, path, value) {
+        let parts = path.split('.');
+        let property = parts.shift();
+
+        if (path === property) {
+            object[property] = value === 'Infinity' ? Infinity : value;
+
+            return;
+        }
+
+        if (isFinite(parts[0])) {
+            object[property] = object[property] || [];
         } else {
-            adapter.setData(data).catch(log);
+            object[property] = object[property] || {};
         }
-    } catch (e) {
-        log(e);
-    }
-});
 
-document.getElementById('exit-btn').addEventListener('click', function () {
-    try {
-        adapter.exit().catch(log);
-    } catch (e) {
-        log(e);
-    }
-});
-
-document.getElementById('add-car-service-btn').addEventListener('click', function () {
-    removeChildrenFromElement('data-form', 'service');
-    form.appendChild(carServiceTemplate.cloneNode(true));
-});
-
-document.getElementById('add-hotel-service-btn').addEventListener('click', function () {
-    removeChildrenFromElement('data-form', 'service');
-    form.appendChild(hotelServiceTemplate.cloneNode(true));
-});
-
-document.getElementById('add-round-trip-service-btn').addEventListener('click', function () {
-    removeChildrenFromElement('data-form', 'service');
-    form.appendChild(roundTripServiceTemplate.cloneNode(true));
-});
-
-document.getElementById('add-camper-service-btn').addEventListener('click', function () {
-    removeChildrenFromElement('data-form', 'service');
-    form.appendChild(camperServiceTemplate.cloneNode(true));
-});
-
-function setValueToPropertyPath(object, path, value) {
-    var parts = path.split('.');
-    var property = parts.shift();
-
-    if (path === property) {
-        object[property] = value === 'Infinity' ? Infinity : value;
-
-        return;
+        setValueToPropertyPath(object[property], parts.join('.'), value);
     }
 
-    if (isFinite(parts[0])) {
-        object[property] = object[property] || [];
-    } else {
-        object[property] = object[property] || {};
+    function doExit() {
+        try {
+            crsAdapter.exit().catch(log);
+        } catch (e) {
+            log(e);
+        }
     }
 
-    setValueToPropertyPath(object[property], parts.join('.'), value);
-}
+    function initFormFields() {
+        let placeholderFields = document.querySelectorAll('[data-form-field]');
+
+        Array.from(placeholderFields).forEach(function(placeholderField) {
+            let formGroup = formFieldTemplate.cloneNode(true);
+            let label = formGroup.childNodes[1];
+            let input = formGroup.childNodes[3];
+
+            formGroup.id = '';
+
+            label.innerHTML = placeholderField.dataset.label;
+            input.name = placeholderField.dataset.name;
+            input.value = placeholderField.dataset.value || '';
+            input.title = placeholderField.dataset.title || '';
+
+            placeholderField.parentNode.replaceChild(formGroup, placeholderField);
+        });
+    }
+})();
