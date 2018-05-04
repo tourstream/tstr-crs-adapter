7fd4cfbe83e7fb9b90fae28667739ac789a068af
Steve Reichenbach <steve.reichenbach@tourstream.eu>
2018-05-04 11:40:03 +0200

    BM-214 remove manual uglify config for prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,10 +10,6 @@ config.plugins = [
     new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production'),
     }),
-    new webpack.optimize.UglifyJsPlugin({
-        minimize: true,
-        sourceMap: true,
-    }),
     new webpack.LoaderOptionsPlugin({
         minimize: true,
     }),
